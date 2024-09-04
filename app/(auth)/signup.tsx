dfdd582e865cb5023676ecab8db57cc846129a75
perhac <abdullah13112004@gmail.com>
2024-09-04 21:19:40 +0530

    chore!: test signup api

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,79 +1,108 @@
-import { View, Text, SafeAreaView, ScrollViewComponent, ViewComponent, Image, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler'
-import {images} from '../../constants'
-import FormFeild from '@/components/FormFeild'
-import CustomButton from '../../components/CustomButton'
-import { Link, router } from 'expo-router'
-import SelectButton from '@/components/SelectButton'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollViewComponent,
+  ViewComponent,
+  Image,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+} from "react-native-gesture-handler";
+import { images } from "../../constants";
+import FormFeild from "@/components/FormFeild";
+import CustomButton from "../../components/CustomButton";
+import { Link, router } from "expo-router";
+import SelectButton from "@/components/SelectButton";
 
-// import { createUser } from '../../lib/appwrite'
+import { useSignup } from "@/hooks/authHooks/useSignup";
+
 const signup = () => {
- const [form,setForm] = useState({username:'',email:'',password:''});
- const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ username: "", email: "", password: "" });
+  const { signUpUser, loading, error } = useSignup();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-//  const submit= async () =>{
-//   if(!form.email||!form.username||!form.password){
-//     Alert.alert('Error','Please fill in all the feilds')
-//   }
-//   setIsSubmitting(true);
-//   try {
-//     const result= await createUser(form.email,form.password,form.username)
+  const handleSignup = async () => {
+    console.log("Sign up...");
+    const signupData = {
+      name: "John Doe",
+      email: "john@example.com",
+      phoneNumber: "1234567890",
+      password: "password123",
+      role: "USER",
+    };
 
-//     router.replace('/home')
+    await signUpUser(signupData);
+  };
 
-//   } catch (error) {
-//     Alert.alert('Error',error.message)
-//   }finally{
-//     setIsSubmitting(false);
-//   }
-    
-//  }
-  const options = ['Admin', 'Guard'];
+  const options = ["Admin", "Guard"];
 
   return (
-    <SafeAreaView className='bg-primary h-full'>
+    <SafeAreaView className="bg-primary h-full">
       <GestureHandlerRootView>
-      <ScrollView>
-        <View className='w-full min-h-[100vh] justify-center px-4 my-6'>
-          <Image source={images.logo} resizeMode='contain' className='w-[200px] h-[85px]' />
-          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>Register to Vers</Text>
-
-          <FormFeild 
-          title='Username'
-          value={form.username} 
-          handleChangeText={(e)=>setForm({...form,username:e})} 
-          otherStyles='mt-7' />
-          <FormFeild 
-          title='Email'
-          value={form.email} 
-          handleChangeText={(e)=>setForm({...form,email:e})} 
-          otherStyles='mt-7' 
-          keyboardType='email-address' />
-          <FormFeild 
-          title='Password'
-          value={form.password} 
-          handleChangeText={(e)=>setForm({...form,password:e})} 
-          otherStyles='mt-7' />
-          <SelectButton title='Select your Role' containerStyle='mt-7' options={options} optionStyle='bg-secondary'/>
-          <CustomButton title='Sign Up' containerStyle='mt-7' 
-          handlePress={()=>router.push('/home')} />
-          <View className='justify-center pt-5 flex-row gap-2'>
-            <Text className='text-lg font-pregular text-gray-100'>
-                Already have an account?
+        <ScrollView>
+          <View className="w-full min-h-[100vh] justify-center px-4 my-6">
+            <Image
+              source={images.logo}
+              resizeMode="contain"
+              className="w-[200px] h-[85px]"
+            />
+            <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+              Register to Vers
             </Text>
-            <Link href='/signin' className='text-secondary font-psemibold text-lg'>SignIn</Link>
+
+            <FormFeild
+              title="Username"
+              value={form.username}
+              handleChangeText={(e) => setForm({ ...form, username: e })}
+              otherStyles="mt-7"
+            />
+            <FormFeild
+              title="Email"
+              value={form.email}
+              handleChangeText={(e) => setForm({ ...form, email: e })}
+              otherStyles="mt-7"
+              keyboardType="email-address"
+            />
+            <FormFeild
+              title="Password"
+              value={form.password}
+              handleChangeText={(e) => setForm({ ...form, password: e })}
+              otherStyles="mt-7"
+            />
+            <SelectButton
+              title="Select your Role"
+              containerStyle="mt-7"
+              options={options}
+              optionStyle="bg-secondary"
+            />
+            <CustomButton
+              title="Sign Up"
+              containerStyle="mt-7"
+              handlePress={handleSignup}
+              isLoading={loading}
+            />
+            <View className="justify-center pt-5 flex-row gap-2">
+              <Text className="text-lg font-pregular text-gray-100">
+                Already have an account?
+              </Text>
+              <Link
+                href="/signin"
+                className="text-secondary font-psemibold text-lg"
+              >
+                SignIn
+              </Link>
+            </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
       </GestureHandlerRootView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default signup
-
-
-
+export default signup;
 
 // com.aman.aora
