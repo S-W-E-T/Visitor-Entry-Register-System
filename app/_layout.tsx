98c2a0da501dc02afaf01a5d972eaaa41c287276
perhac <abdullah13112004@gmail.com>
2024-09-04 21:18:01 +0530

    feat!: toast

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import {Slot,Stack} from 'expo-router'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Toast from "react-native-toast-message";
+import { Slot, Stack } from "expo-router";
 
 const _layout = () => {
   return (
-    <Stack>
-        <Stack.Screen name="index" options={{headerShown:false}}/>
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         {/* <Stack.Screen name="Collapsible" options={{headerShown:false}}/> */}
-        <Stack.Screen name='(auth)' options={{headerShown:false}}/>
-        <Stack.Screen name='(tabs)' options={{headerShown:false}}/>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         {/* <Stack.Screen name='/Search/[query]' options={{headerShown:false}}/> */}
-    </Stack>
-  )
-}
+      </Stack>
+      <Toast />
+    </>
+  );
+};
 
-export default _layout
+export default _layout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
