959937dff64d63ddf60b137b5d6f4d414f0c0288
PerHac13 <abdullah13112004@gmail.com>
2024-10-29 20:11:24 +0530

    fix!: disable signup while requesting

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,15 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native";
+import React from "react";
 
-const CustomButton = ({title,handlePress,containerStyle,textStyle,isLoading}) => {
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyle,
+  textStyle,
+  isLoading,
+}) => {
   return (
-    <TouchableOpacity onPress={handlePress} 
-    className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyle} ${isLoading?'opacity-50':''}`}
-    activeOpacity={0.7}>
-      <Text className={`text-primary font-psemibold text-lg ${textStyle}`}>{title}</Text>
+    <TouchableOpacity
+      onPress={handlePress}
+      disabled={isLoading}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyle} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      activeOpacity={0.7}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyle}`}>
+        {title}
+      </Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 export default CustomButton;
